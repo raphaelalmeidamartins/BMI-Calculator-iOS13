4e3203f1217235292b126b90ef218af7cf2e93ec
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-07-09 17:49:09 -0300

    :bug: fix: change to have one IBAction per UISlider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func slideValueChanged(_ sender: UISlider) {
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
         print(sender.value)
     }
 }
