ea7493a9165940edef6ffb9d09f199d7f65df7d7
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-07-09 15:08:40 -0300

    :sparkles: feat: complete challenge of printing out the values of the sliders when they are changed

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func slideValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 }
 
